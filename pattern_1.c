340eff5b8a10c26a86cbd87c89c0b4870377ddc4
srayo16 <srayosikder@gmail.com>
2023-04-30 00:35:11 +0600

    pattern

---
+++ pattern_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int k = 1; k <= i; k++)
+        {
+            printf("zijad magi ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
